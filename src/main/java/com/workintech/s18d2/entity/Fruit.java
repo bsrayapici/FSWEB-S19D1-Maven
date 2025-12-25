71d86f8e09dd51387ce52a0b5950d102db1610b0
busra <37024872+bsrayapici@users.noreply.github.com>
2025-12-25 18:55:06 +0300

    manuuel constructor eklendi

--- src/main/java/com/workintech/s18d2/entity/Fruit.java
+++ src/main/java/com/workintech/s18d2/entity/Fruit.java
@@ -3,62 +3,54 @@ package com.workintech.s18d2.entity;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "fruit", schema = "fsweb")
+@Table(name = "fruit")
 public class Fruit {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name")
     private String name;
 
-    @Column(name = "price")
     private Double price;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "fruit_type")
     private FruitType fruitType;
 
-    // Parametresiz constructor
+    // ZORUNLU: JPA + testler için
     public Fruit() {
     }
 
-    // Parametreli constructor
-    public Fruit(Long id, String name, Double price, FruitType fruitType) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.fruitType = fruitType;
-    }
+    // ---------- GETTERS ----------
 
-    // Getter ve Setter metodları
     public Long getId() {
         return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public Double getPrice() {
         return price;
     }
 
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
     public FruitType getFruitType() {
         return fruitType;
+    }
+
+    // ---------- SETTERS ----------
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
     }
 
     public void setFruitType(FruitType fruitType) {
--- src/main/java/com/workintech/s18d2/entity/Vegetable.java
+++ src/main/java/com/workintech/s18d2/entity/Vegetable.java
@@ -3,64 +3,58 @@ package com.workintech.s18d2.entity;
 import jakarta.persistence.*;
 
 @Entity
-@Table(name = "vegetable", schema = "fsweb")
+@Table(name = "vegetable")
 public class Vegetable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name")
     private String name;
 
-    @Column(name = "price")
     private Double price;
 
     @Column(name = "is_grown_on_tree")
-    private Boolean isGrownOnTree;
+    private boolean grownOnTree;
 
-    // Parametresiz constructor
+    // ZORUNLU
     public Vegetable() {
     }
 
-    // Parametreli constructor
-    public Vegetable(Long id, String name, Double price, Boolean isGrownOnTree) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.isGrownOnTree = isGrownOnTree;
-    }
+    // ---------- GETTERS ----------
 
-    // Getter ve Setter metodları
     public Long getId() {
         return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public Double getPrice() {
         return price;
+    }
+
+    // DİKKAT: test isGrownOnTree() çağırıyor
+    public boolean isGrownOnTree() {
+        return grownOnTree;
+    }
+
+    // ---------- SETTERS ----------
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public void setPrice(Double price) {
         this.price = price;
     }
 
-    public Boolean isGrownOnTree() {
-        return isGrownOnTree;
-    }
-
-    public void setGrownOnTree(Boolean grownOnTree) {
-        isGrownOnTree = grownOnTree;
+    public void setGrownOnTree(boolean grownOnTree) {
+        this.grownOnTree = grownOnTree;
     }
 }
